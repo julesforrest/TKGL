3c7a73959224a180ec16d142934a3dfd3646b93a
Jules Forrest <julesforrest@gmail.com>
2020-05-16 18:40:49 -0700

    loading

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,31 @@ const Home = () => {
       >
         <button type="submit">
           {buttonLoading ? (
-            "Loading..."
+            <>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="icon spinner"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 2L12 6"></path>
+                <path d="M12 18L12 22"></path>
+                <path d="M4.93 4.93L7.76 7.76"></path>
+                <path d="M16.24 16.24L19.07 19.07"></path>
+                <path d="M2 12L6 12"></path>
+                <path d="M18 12L22 12"></path>
+                <path d="M4.93 19.07L7.76 16.24"></path>
+                <path d="M16.24 7.76L19.07 4.93"></path>
+              </svg>
+              Adding items to list
+            </>
           ) : (
             <>
               <svg
@@ -111,6 +135,19 @@ const Home = () => {
         }
         * {
           box-sizing: border-box;
+        }
+
+        @-webkit-keyframes rotation {
+          from {
+            -webkit-transform: rotate(0deg);
+          }
+          to {
+            -webkit-transform: rotate(359deg);
+          }
+        }
+
+        .spinner {
+          -webkit-animation: rotation 3s infinite linear;
         }
       `}</style>
     </div>
